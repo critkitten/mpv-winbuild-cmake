93d562bf1f702ab7a6e1dde351453da4fe92e731
critkitten <84600276+critkitten@users.noreply.github.com>
2023-03-15 16:17:58 +0100

    add configure.sh

--- configure.sh
+++ configure.sh
@@ -5,4 +5,16 @@ cmake -DTARGET_ARCH=x86_64-w64-mingw32 -G Ninja ..
 
 echo "build toolchain and mpv"
 echo "ninja gcc"
+#!/bin/bash
+
+echo "pacman -S git gyp mercurial subversion ninja cmake meson ragel yasm nasm asciidoc enca gperf unzip p7zip gcc-multilib clang python-pip curl lib32-glib2"
+echo "pip3 install rst2pdf mako jsonschema"
+
+mkdir build64
+cd build64
+
+cmake -DTARGET_ARCH=x86_64-w64-mingw32 -G Ninja ..
+
+echo "ninja gcc"
 echo "ninja mpv"
+
