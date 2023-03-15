354fb079cc9fb2c2387fb463b80245f29d2dbc96
critkitten <84600276+critkitten@users.noreply.github.com>
2023-03-15 16:22:39 +0100

    need git config

--- configure.sh
+++ configure.sh
@@ -10,6 +10,9 @@ echo "ninja gcc"
 echo "pacman -S git gyp mercurial subversion ninja cmake meson ragel yasm nasm asciidoc enca gperf unzip p7zip gcc-multilib clang python-pip curl lib32-glib2"
 echo "pip3 install rst2pdf mako jsonschema"
 
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
 mkdir build64
 cd build64
 
